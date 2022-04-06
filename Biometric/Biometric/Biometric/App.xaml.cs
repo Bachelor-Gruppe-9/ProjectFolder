5ee5df1cfeb4c16e88cc4baf9986cb20c593e461
Tommy <Tommyhdt@gmail.com>
2022-04-06 16:25:48 +0200

    permissions

--- Biometric/Biometric/Biometric/App.xaml.cs
+++ Biometric/Biometric/Biometric/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using static Xamarin.Essentials.Permissions;
 
 namespace Biometric
 {
@@ -15,7 +17,50 @@ namespace Biometric
 
         protected override void OnStart()
         {
+            if (Xamarin.Forms.Device.RuntimePlatform == Xamarin.Forms.Device.iOS)
+            {
+                //AskForRelevantPermissionsAsync();
+                //AskForRelevantPermissionsAsync().GetAwaiter().GetResult();
+                //Mainthread virker ikke bra på IOS det kan kræsje
+            }
+            else if (Xamarin.Forms.Device.RuntimePlatform == Xamarin.Forms.Device.Android)
+            {
+                MainThread.BeginInvokeOnMainThread(async () =>
+                {
+                    await AskForRelevantPermissionsAsync();
+                });
+            }
         }
+
+        private async Task AskForRelevantPermissionsAsync()
+        {
+
+            await AskforPermissionAsync<Permissions.LocationWhenInUse>();
+            await AskforPermissionAsync<Permissions.LocationAlways>();
+
+
+        }
+
+        private async Task AskforPermissionAsync<TPermission>()
+            where TPermission : BasePermission, new()
+        {
+            var result = await CheckStatusAsync<TPermission>();
+            if (result != PermissionStatus.Granted)
+                await RequestAsync<TPermission>();
+        }
+
+        public async Task<PermissionStatus> CheckAndRequestPermissionAsync<T>(T permission)
+            where T : BasePermission
+        {
+            var status = await permission.CheckStatusAsync();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await permission.RequestAsync();
+            }
+
+            return status;
+        }
+
 
         protected override void OnSleep()
         {
