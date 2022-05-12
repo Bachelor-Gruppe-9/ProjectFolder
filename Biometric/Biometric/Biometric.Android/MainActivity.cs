d09c823c1303d46d343b6ec606941ed0081d706e
Tommy <Tommyhdt@gmail.com>
2022-05-12 15:05:29 +0200

    in progress splash screen

--- Biometric/Biometric/Biometric.Android/MainActivity.cs
+++ Biometric/Biometric/Biometric.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Plugin.LocalNotification;
 
 namespace Biometric.Droid
 {
-    [Activity(Label = "Biometric", Icon = "@mipmap/LockTap_adaptive_fore", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Biometric", Icon = "@mipmap/LockTap_adaptive_fore", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
